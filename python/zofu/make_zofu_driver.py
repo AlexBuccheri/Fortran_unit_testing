80ed0bf1e73ef6c5658dfc3a1d6a178ed6a9b81b
Alex Buccheri <alexanderbuccheri@googlemail.com>
2021-02-09 16:45:57 +0000

    Driver generator generalised to MPI

--- python/zofu/make_zofu_driver.py
+++ python/zofu/make_zofu_driver.py
@@ -21,19 +21,25 @@ __license__ = "GPL"
 __email__ = "abuccheri@physik.hu-berlin.de"
 
 import sys
+import argparse
+import os.path
 
 def parse_fortran_module(file_name: str) -> tuple:
     """
     Given a fortran file, extract the module name,
     any subroutines beginning with test_ (indicating a unit test)
     and the first line below each subroutine name (if a comment)
+
     :param file_name: file name
     :return: module_name, subroutine = list of dicts,
         {'name':'test_name', 'comment':'test comment'}
     """
 
+    if not os.path.isfile(file_name):
+        print("File: %s does not exist" % file_name)
+        sys.exit()
+
     fid = open(file_name, 'r')
-    # TODO(Alex) Check file exists
     lines = fid.readlines()
     fid.close()
 
@@ -67,7 +73,7 @@ def parse_fortran_module(file_name: str) -> tuple:
             subroutines.append({'name': routine_name,
                                 'comment': test_comment})
 
-    # TODO(Alex) Would really like to say which one
+    # TODO(Alex) Would really like to say which one - check out numpy's assert
     assert module_name is not None, "One or more.f90 files do not contain a module"
 
     return module_name, subroutines
@@ -93,13 +99,14 @@ def parse_fortran_source(fortran_source: list):
     return modules, subroutines
 
 
-def generate_zofu_driver(driver_name: str, modules: list, subroutines: list) -> str:
+def generate_zofu_driver(driver_name: str, modules: list, subroutines: list, use_mpi: bool) -> str:
     """
     Generate the Zofu test driver program
-    #TODO(Alex) Make sure this works for MPI too => Change use zofu and stop statements
+
     :param driver_name: string, driver name
     :param modules: list of module names
     :param subroutines: list of dictionaries. Each dictionary should contain keys = ['name', 'comment']
+    :param use_mpi: bool, Unit tests are serial or MPI
     :return: Zofu test driver string
     """
     indent = ' '*3
@@ -113,14 +120,17 @@ def generate_zofu_driver(driver_name: str, modules: list, subroutines: list) -> 
     if driver_name[-4:].lower() in extensions:
         driver_name = driver_name[:-4]
 
+    zofu_module = 'use zofu_mpi \n' if use_mpi else 'use zofu \n'
+    zofu_type = 'unit_test_mpi_type' if use_mpi else 'unit_test_type'
+
     driver_string = 'program ' + driver_name + '\n'
-    driver_string += indent + 'use zofu \n'
+    driver_string += indent + zofu_module
 
     for module in modules:
         driver_string += indent + 'use ' + module + '\n'
 
     driver_string += indent + 'implicit none \n'
-    driver_string += indent + 'type(unit_test_type) :: test \n\n'
+    driver_string += indent + 'type(' + zofu_type + ') :: test \n\n'
     driver_string += indent + 'call test%init() \n\n'
 
     for subroutine in subroutines:
@@ -128,30 +138,64 @@ def generate_zofu_driver(driver_name: str, modules: list, subroutines: list) -> 
                          + subroutine['comment'] + '") \n\n'
 
     driver_string += indent + 'call test%summary() \n\n'
+    # TODO(Alex) Consider stopping with mpi_abort if MPI
     driver_string += indent + 'if (test%failed) stop 1 \n\n'
     driver_string += 'end program ' + driver_name
 
     return driver_string
 
 
+def parse_cmd_line_args() -> tuple:
+    """
+    Parse command-line options
+    :return: command-line options: mpi (logical), modules (list), driver (str)
+    """
+
+    description =  \
+        "Usage: python3 -mpi -mod make_zofu_driver.py module1.f90 module2.F90 ... -driver test_driver.f90"
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('-mpi',
+                        help='Indicate that the tests are MPI-based. '
+                             'If the option is excluded, the default is serial',
+                        dest='use_mpi',
+                        default=False,
+                        action='store_true')
+
+    parser.add_argument('-mod',
+                        dest='module_files',
+                        help="Module files containing unit tests.",
+                        type=str,
+                        nargs='+')
+
+    parser.add_argument('-driver',
+                        dest='full_path_driver_name',
+                        help="Name for the test driver that runs the unit "
+                             "tests defined in the specified modules.",
+                        type=str,
+                        default='test_driver.f90')
+
+    args = parser.parse_args()
+    assert len(args.module_files) > 0, \
+        "Requires at least one command-line argument for a test module"
+
+    return args.use_mpi, args.module_files, args.full_path_driver_name
+
+
 def main():
     """
     Parse fortran unit test modules and return a test driver
     that calls each subroutine prefixed with 'test_'.
-    See description of script for more details. Call with:
-
-    python3 make_zofu_driver.py module1.f90 module2.F90 test_driver.f90
+    See description of script for more details.
     """
-    assert len(sys.argv) >= 3, \
-        'Must provide at least one module and the test driver name as script args'
-    fortran_source = sys.argv[1:-1]
-    driver_name_with_path = sys.argv[-1]
-    driver_name = driver_name_with_path.split('/')[-1]
+    use_mpi, fortran_source, full_path_driver_name = parse_cmd_line_args()
+    driver_name = full_path_driver_name.split('/')[-1]
 
     modules, subroutines = parse_fortran_source(fortran_source)
-    driver_string = generate_zofu_driver(driver_name, modules, subroutines)
+    driver_string = generate_zofu_driver(driver_name, modules, subroutines, use_mpi)
 
-    fid = open(driver_name_with_path, "w")
+    fid = open(full_path_driver_name, "w")
     fid.write(driver_string)
     fid.close()
     return
